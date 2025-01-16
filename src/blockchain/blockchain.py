f9fc422628d5214d077beec89c74fc0de73b2120
Benhur P Balaji <benhur@thelinuxgeek.net>
2025-01-16 17:30:23 -0500

    Update blockchain.py

--- src/blockchain/blockchain.py
+++ src/blockchain/blockchain.py
@@ -1,1 +1,30 @@
-# blockchain.py placeholder
+from .block import Block
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.unconfirmed_transactions = []
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        genesis_block = Block(0, "0", [])
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+
+    def add_block(self, block, proof):
+        previous_hash = self.last_block.hash
+        if previous_hash != block.previous_hash or not self.is_valid_proof(block, proof):
+            return False
+
+        block.hash = proof
+        self.chain.append(block)
+        return True
+
+    @staticmethod
+    def is_valid_proof(block, block_hash):
+        return block_hash.startswith("0000") and block_hash == block.compute_hash()
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
