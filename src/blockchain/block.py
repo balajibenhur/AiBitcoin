d045ccd496ffce1436c1a8f752b55f7b55729bc6
Benhur P Balaji <benhur@thelinuxgeek.net>
2025-01-16 17:29:14 -0500

    Update block.py

--- src/blockchain/block.py
+++ src/blockchain/block.py
@@ -1,1 +1,15 @@
-# block.py placeholder
+import hashlib
+import time
+
+class Block:
+    def __init__(self, index, previous_hash, transactions, timestamp=None):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.transactions = transactions
+        self.timestamp = timestamp or time.time()
+        self.nonce = 0
+
+    def compute_hash(self):
+        block_string = f"{self.index}{self.previous_hash}{self.transactions}{self.timestamp}{self.nonce}"
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
